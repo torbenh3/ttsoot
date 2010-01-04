d4a2d4a8944e32090f01f5547a87249d81a12bcc
Torben Hohn <torbenh@gmx.de>
2010-01-04 17:57:35 +0100

    file restructure part 1

--- containers.h
+++ containers.h
@@ -14,28 +14,6 @@ class Block
 	virtual void register_params( paramMap &map, std::string prefix ) = 0;
 };
 
-class SinOsc : public Block
-{
-    private:
-	float _sin;
-	float _cos;
-	float _omega;
-    public:
-	SinOsc();
-	inline float process() {
-	    _sin = _sin + _omega * _cos;
-	    _cos = _cos - _omega * _sin;
-	    return  _sin;
-	}
-
-	inline void reset() {
-	    _sin = 0.0;
-	    _cos = 1.0;
-	}
-
-	virtual void register_params( paramMap &map, std::string prefix );
-};
-
 
 template<typename ... Args>
 class Mixer;
